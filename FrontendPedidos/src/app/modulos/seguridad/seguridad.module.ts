98daeabc8db75aab89684b253004b140c8676258
wespitia9 <90352551+wilsonespitia@users.noreply.github.com>
2021-12-03 21:36:44 -0500

    Creación de componentes de seguridad.
    Creación de componentes de seguridad.

--- FrontendPedidos/src/app/modulos/seguridad/seguridad.module.ts
+++ FrontendPedidos/src/app/modulos/seguridad/seguridad.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SeguridadRoutingModule } from './seguridad-routing.module';
+import { IdentificacionComponent } from './identificacion/identificacion.component';
+import { CambioClaveComponent } from './cambio-clave/cambio-clave.component';
+import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IdentificacionComponent,
+    CambioClaveComponent,
+    RecuperarClaveComponent
+  ],
   imports: [
     CommonModule,
     SeguridadRoutingModule
