b4a870b7616e61b331dc17fc97424465623f708c
wespitia9 <90352551+wilsonespitia@users.noreply.github.com>
2021-12-03 23:30:29 -0500

    Identificar

--- FrontendPedidos/src/app/servicios/seguridad.service.ts
+++ FrontendPedidos/src/app/servicios/seguridad.service.ts
@@ -1,10 +1,26 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ModeloIdentificar } from '../modelos/identificar.modelo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeguridadService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+
+   }
+
+   Identificar(usuario: string, clave: string): Observable<ModeloIdentificar>{
+     return this.http.post<ModeloIdentificar>("localhost:3000/identificar-persona",{
+       usuario: usuario,
+       clave: clave
+     },{
+       headers: new HttpHeaders({
+
+       })
+     })
+
+   }
 }
