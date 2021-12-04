41a35fe018f62b89ceaf48c674654a8ef946469c
wespitia9 <90352551+wilsonespitia@users.noreply.github.com>
2021-12-03 21:38:03 -0500

    Componentes CRUD para personas
    Componentes CRUD para personas

--- FrontendPedidos/src/app/modulos/administracion/administracion.module.ts
+++ FrontendPedidos/src/app/modulos/administracion/administracion.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdministracionRoutingModule } from './administracion-routing.module';
+import { CrearPersonaComponent } from './personas/crear-persona/crear-persona.component';
+import { EditarPersonaComponent } from './personas/editar-persona/editar-persona.component';
+import { EliminarPersonaComponent } from './personas/eliminar-persona/eliminar-persona.component';
+import { BuscarPersonaComponent } from './personas/buscar-persona/buscar-persona.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CrearPersonaComponent,
+    EditarPersonaComponent,
+    EliminarPersonaComponent,
+    BuscarPersonaComponent
+  ],
   imports: [
     CommonModule,
     AdministracionRoutingModule
