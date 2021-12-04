db6ee5b439ab68160ec10c3ead97421b67100a32
wespitia9 <90352551+wilsonespitia@users.noreply.github.com>
2021-12-03 21:42:04 -0500

    Creación de carpeta plantilla.
    Barra de navegación y pie de página.

--- FrontendPedidos/src/app/app.module.ts
+++ FrontendPedidos/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
+import { PiePaginaComponent } from './plantilla/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacionComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
