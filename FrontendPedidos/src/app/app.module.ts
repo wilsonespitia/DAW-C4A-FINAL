25f21ff22e6532292a8720e04cadd2726dbd9484
wespitia9 <90352551+wilsonespitia@users.noreply.github.com>
2021-12-03 21:45:14 -0500

    Update app.module.ts

--- FrontendPedidos/src/app/app.module.ts
+++ FrontendPedidos/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrearPersonaComponent } from './modulos/administracion/personas/crear-persona/crear-persona.component';
 import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
 import { PiePaginaComponent } from './plantilla/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CrearPersonaComponent,
     BarraNavegacionComponent,
     PiePaginaComponent
   ],
