a2da43055ca635a798f5ac3e9f9cb758e72ce887
wespitia9 <90352551+wilsonespitia@users.noreply.github.com>
2021-12-05 20:28:22 -0500

    Update identificacion.component.ts

--- FrontendPedidos/src/app/modulos/seguridad/identificacion/identificacion.component.ts
+++ FrontendPedidos/src/app/modulos/seguridad/identificacion/identificacion.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SeguridadService } from 'src/app/servicios/seguridad.service';
 import * as CryptoJS from 'crypto-js';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-identificacion',
@@ -16,7 +17,8 @@ export class IdentificacionComponent implements OnInit {
   })
 
   constructor(private fb: FormBuilder,
-    private servicioSeguridad: SeguridadService) { }
+    private servicioSeguridad: SeguridadService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -27,6 +29,7 @@ export class IdentificacionComponent implements OnInit {
     let claveCifrada = CryptoJS.MD5(clave).toString();
     this.servicioSeguridad.Identificar(usuario, claveCifrada).subscribe((datos:any)=>{
       this.servicioSeguridad.AlmacenarSesion(datos);
+      this.router.navigate(["/inicio"]);
     }, (error: any)=>{
       //KO
       alert("Datos Invalidos")
